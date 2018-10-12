b261aff5bccc44ce5daf8a59a026a6da71950727
giuseppe-dandrea <gdandrea97@gmail.com>
2018-10-12 13:41:55 +0200

    Initial commit

---
+++ i3-switch-if-workspace-empty.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+import i3ipc
+from collections import deque
+
+i3 = i3ipc.Connection()
+
+# Circular buffer containing last used workspaces
+WORKSPACES_LIST = deque(maxlen = 10)
+
+def is_workspace_empty(workspace_name):
+	workspace = i3.get_tree().find_named(workspace_name)
+	if not workspace:
+		return True
+	if workspace[0].workspace().leaves():
+		print(workspace[0].workspace().leaves())
+		return False
+	else:
+		return True
+
+# On workspace focus, add the workspace to the circular buffer
+def on_workspace_focus(self, e):
+	if e.current:
+		WORKSPACES_LIST.append(e.current.name)
+
+# On window close, if the workspace is empty
+# go back to the first non empty workspace
+def on_window_close(self, e):
+	focused = i3.get_tree().find_focused()
+	if not focused.workspace().leaves():
+		# If workspace empty
+		for w in reversed(WORKSPACES_LIST):
+			if not is_workspace_empty(w):
+				i3.command('workspace ' + w)
+				break
+
+# Subscribing to events
+i3.on('workspace::focus', on_workspace_focus)
+i3.on('window::close', on_window_close)
+
+# Start the main loop and wait for events to come in.
+i3.main()
